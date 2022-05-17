f2a68e7d126521bae2dc169142a9f06431649683
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-17 07:13:47 +0700

    add router todos

--- api/routers/router.ts
+++ api/routers/router.ts
@@ -1,6 +1,10 @@
 import {Router} from '../deps.ts'
-import {rootHandler,todoHandler} from '../middleware/index.ts'
+import {rootHandler,todosHandler} from '../controllers/index.ts'
 
 export const router = new Router();
 router.get('/',rootHandler.getHome);
-router.get('/todo',todoHandler.getTodos)
+router.get('/todos',todosHandler.getTodos)
+router.get('/todos/:id',todosHandler.get)
+router.post('/todos',todosHandler.create)
+router.put('/todos/:id',todosHandler.update)
+router.delete('/todos:id',todosHandler.remove)
