92ba369b22f190236f8ecbf814c5463d290dbc86
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-17 10:13:30 +0700

    add Register in router

--- api/routers/router.ts
+++ api/routers/router.ts
@@ -1,11 +1,14 @@
 import {Router} from '../deps.ts'
 import {rootHandler,todosHandler} from '../controllers/index.ts'
+import {Register} from '../controllers/auth.controller.ts'
 
 const router = new Router();
 router.get("/api",rootHandler.getHome);
 router.get('/api/todos',todosHandler.getTodos)
 router.get('/api/todos/:id',todosHandler.get)
+
 router.post('/api/todos',todosHandler.create)
+router.post('/api/register',Register)
 router.put('/api/todos/:id',todosHandler.update)
 router.delete('/api/todos:id',todosHandler.remove)
 
