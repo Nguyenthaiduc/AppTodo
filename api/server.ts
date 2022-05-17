891cced1004860bed643980693459fb3eb95e0b1
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-17 08:26:09 +0700

    add server

--- api/server.ts
+++ api/server.ts
@@ -1,2 +1,18 @@
 import {Application,bold,yellow} from './deps.ts'
-import {router} from './routers'
+import {router} from './routers/router.ts'
+import {logger,errorHandler} from './middleware/index.ts'
+
+const app = new Application();
+
+app.use(logger);
+app.use(errorHandler);
+app.use(router.routes);
+app.use(router.allowedMethods());
+
+app.addEventListener("listen",({hostname,port})=> {
+    console.log(bold(`Start Listening on ${hostname || "localhost"}`)+yellow(`:${port}`))
+})
+
+const port = parseInt(Deno.env.get("PORT") || "8000");
+
+app.listen({port : port});
