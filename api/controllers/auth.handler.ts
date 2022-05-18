eb53d7d19488cc55b7522eb426c44be1a08c3752
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-18 15:48:55 +0700

    authHandler in controller

--- api/controllers/auth.handler.ts
+++ api/controllers/auth.handler.ts
@@ -1,10 +1,22 @@
-import { RouterContext,bcrypt} from '../deps.ts'
+import { RouterContext,bcrypt,Status} from '../deps.ts'
 import { User } from '../models/user.ts'
+import { UserRepository} from '../repositories/user.repository.ts'
+import { JwtService } from '../service/jwt.service.ts'
 
 
 export const Register = async ({request,response} : RouterContext) => {
     const body = await request.body().value;
     const user = new User();
+
+    user.first_name = body.first_name;
+    user.last_name = body.last_name;
+    user.email = body.email;
+    user.password = body.password
+
+    const userRepository = new UserRepository();
+    await userRepository.create(user);
+
+    response.body = "Created Successfully !"
 
 
     
@@ -12,6 +24,36 @@ export const Register = async ({request,response} : RouterContext) => {
 
 export const Login = async ({request,response} : RouterContext) => {
     const [email,password] = await request.body().value;
+
+    const userRepository = new UserRepository
+    const [user,error] = await userRepository.findByEmail(email);
+    console.log(user,error);
+
+    if(error) {
+        response.status = Status.BadRequest;
+        response.body = {
+            messgae: error,
+        }
+        return;
+    }
+
+
+    if(!user) {
+        response.status = Status.BadRequest;
+        response.body = {
+            message : "USer not Found"
+        }
+        return;
+    }
+
+    const jwtService = new JwtService()
+    const jwt = await jwtService.create(user.id);
+
+    response.status = Status.OK;
+    response.body = {
+        jwt,
+        }
+    }
 
     
 
