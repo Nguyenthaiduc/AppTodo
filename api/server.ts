6214aec43b387adbeaf3a1a2a5e20e5ae5222837
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-21 19:48:17 +0700

    fix server

--- api/server.ts
+++ api/server.ts
@@ -2,21 +2,25 @@ import {Application,bold,yellow} from './deps.ts'
 import router from './routers/router.ts'
 import {logger,internalServerErrorHandler,notFoundErrorHandler} from './middleware/index.ts'
 
-const app = new Application();
 
-app.use(logger);
-app.use(internalServerErrorHandler);
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.use(notFoundErrorHandler)
+const listenAndServe = () => {
+    
+    const app = new Application();
+    
+    app.use(logger);
+    app.use(internalServerErrorHandler);
+    app.use(router.routes());
+    app.use(router.allowedMethods());
+    app.use(notFoundErrorHandler)
 
 
-
-
-app.addEventListener("listen",({hostname,port})=> {
+    app.addEventListener("listen",({hostname,port})=> {
     console.log(bold(`Start Listening on ${hostname || "localhost"}`)+yellow(`:${port}`))
 })
 
 const port = parseInt(Deno.env.get("PORT") || "8000");
 
 app.listen({port : port});
+}
+
+listenAndServe();
