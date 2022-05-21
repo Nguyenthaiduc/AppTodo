5ec34b4abd8a6f09ab94511830c7385873155960
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-21 20:14:26 +0700

    refactor

--- api/controllers/auth.handler.ts
+++ api/controllers/auth.handler.ts
@@ -1,80 +1,91 @@
 import { RouterContext,bcrypt,Status} from '../deps.ts'
 import { User } from '../models/user.ts'
 import { UserRepository} from '../repositories/user.repository.ts'
-import { TodoRepository } from '../repositories/todo.repository.ts'
 import { JwtService } from '../service/jwt.service.ts'
 
 
-export const Register = async ({request,response} : RouterContext) => {
-    const body = await request.body().value;
-    const user = new User();
-
-    user.first_name = body.first_name;
-    user.last_name = body.last_name;
-    user.email = body.email;
-    user.password = body.password;
-
-    const userRepository = new UserRepository();
-    await userRepository.create(user);
-
-    response.body = "Created Successfully !"
-
+export class AuthHandler {
+    constructor(private userRepository: UserRepository,
+                private jwtService: JwtService ){}
 
     
+        async signup({request,response} : RouterContext) : Promise<void> {
+            const body = await request.body().value;
+            const user = new User();
+
+            user.first_name = body.first_name;
+            user.last_name = body.last_name;
+            user.email = body.email;
+            user.password = await bcrypt.hash(body.password);
+
+            const userRepository = new UserRepository();
+            await userRepository.create(user);
+
+            response.body = "Created"
+        }
+
+        async login({request,response,cookies} : RouterContext) : Promise<void> {
+            const {email,password} = await request.body().value;
+
+            const [user,error] = await this.userRepository.findByEmail(email);
+            if(error) {
+                response.status = Status.BadRequest;
+                response.body = {
+                    message: error
+                }
+                return
+            }
+            if(!user) {
+                response.status = Status.BadRequest;
+                response.body = {
+                    message: "User not found"
+                }
+                return
+            }
+            if(!bcrypt.compareSync(password, user.password)) {
+                response.status = Status.Unauthorized;
+                response.body = {
+                    message: "Unauthorized"
+                }
+                return
+            }
+
+            const jwtService = new JwtService();
+            const jwt = await jwtService.create(user.id);
+
+            cookies.set('jwt',jwt,{httpOnly: true})
+
+            response.status = Status.OK;
+            response.body = {
+                jwt
+            }
+           
+        }
+
+        logout({response,cookies } : RouterContext): void {
+            cookies.delete('jwt');
+            response.status = Status.OK;
+            response.body = {
+                message: "Log out successfully"
+            }
+
+        }
+
+        async getUser({response,cookies} : RouterContext) : Promise<void> {
+            const [payload,error] = await this.jwtService.verify(cookies.get('jwt') || '');
+            if(error) {
+                response.status = Status.Unauthorized;
+                response.body = {
+                    message: "Unauthorized"
+                }
+                return
+            }
+
+            response.status = Status.OK;
+            response.body = {
+                payload
+            }
+        }
+
+
 }
-
-export const Login = async ({request,response,cookies} : RouterContext) => {
-    const [email,password] = await request.body().value;
-
-    const userRepository = new UserRepository();
-    const [user,error] = await userRepository.findByEmail(email);
-    console.log(user,error);
-
-    if(error) {
-        response.status = Status.BadRequest;
-        response.body = {
-            messgae: error,
-        }
-        return;
-    }
-
-
-    if(!user) {
-        response.status = Status.BadRequest;
-        response.body = {
-            message : "USer not Found"
-        }
-        return;
-    }
-
-    const jwtService = new JwtService()
-    const jwt = await jwtService.create(user.id);
-    //check jwt
-    cookies.set('jwt', jwt, {httpOnly: true});
-
-    response.status = Status.OK;
-    response.body = {
-        jwt,
-        }
-    }
-
-export const Authenticate = async ({response,cookies} : RouterContext) => {
-
-    const jwtService = new JwtService();
-    const [payload,error] = await jwtService.verify(cookies.get('jwt') || '')
-
-    if(error) {
-        response.status = Status.BadRequest;
-        response.body = {
-            message: "Unauthorized"
-        }
-        return;
-    }
-
-    response.status = Status.OK;
-    response.body = {
-        payload,
-    }
-}
-    
-
