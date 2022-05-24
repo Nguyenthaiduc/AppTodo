a2daa4634e3c23c5aac614280556580b39e9491b
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-24 14:29:26 +0700

    add module routers directory

--- api/routers/router.ts
+++ api/routers/router.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from '../middleware/auth.middleware.ts'
 import { loginValidation, registerValidation } from "../validations/index.ts";
 import { TodoRepository, UserRepository } from '../repositories/index.ts';
 import { JwtUtils } from '../service/index.ts';
-import { TodoService } from '../service/index.ts'
+import { TodoService, AuthService  } from '../service/index.ts'
 
 const router = new Router();
 const todoHandler = new TodoHandler(new TodoService(new TodoRepository()), new JwtUtils());
@@ -23,7 +23,8 @@ router.delete('/v1/todos:id',authMiddleware,(ctx)=> todoHandler.remove(ctx))
 
 
 //Authenticate
-const authHandler = new AuthHandler(new UserRepository(), new JwtUtils());
+const authHandler = new AuthHandler(new AuthService(new UserRepository()), new JwtUtils());
+
 router.post('/v1/signup',  registerValidation.RegisterValidation, (ctx) => authHandler.signup(ctx));
 router.post('/v1/login',  loginValidation.LoginValidation, (ctx) => authHandler.login(ctx));
 router.post('/v1/logout', (ctx) => authHandler.logout(ctx));
