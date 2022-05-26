2f9def9e872ab4367623fa3f1ba3422c6543f70c
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-26 10:02:24 +0700

    add update delete Todo in controller

--- express/controllers/todo.controller.ts
+++ express/controllers/todo.controller.ts
@@ -6,68 +6,123 @@ import { ResponseData, ResponseError } from '../interfaces';
 import { Todo } from '../models';
 
 interface Pagination {
-    page : number;
-    limit: number;
+	page: number;
+	limit: number;
 }
 //get All Todo
-export const getTodoList = async(req: IUserRequest,res : Response)=> {
-    try {
-        const pagination = req.query as unknown as Pagination;
-        pagination.limit = pagination.limit ? parseInt(pagination.limit.toString()) : 10 ;
-        pagination.page = pagination.page ? parseInt(pagination.page.toString()) : 1 ;
+export const getTodoList = async (req: IUserRequest, res: Response) => {
+	try {
+		const pagination = req.query as unknown as Pagination;
+		pagination.limit = pagination.limit
+			? parseInt(pagination.limit.toString())
+			: 10;
+		pagination.page = pagination.page
+			? parseInt(pagination.page.toString())
+			: 1;
 
-        const userId = req.userId;
-        const todoRepo = getMongoRepository(Todo);
-        const todoList: Array<Todo> | [] = (
+		const userId = req.userId;
+		const todoRepo = getMongoRepository(Todo);
+		const todoList: Array<Todo> | [] = (
 			await todoRepo.find({ where: { userId: userId } })
 		).slice(
 			(pagination.page - 1) * pagination.limit,
 			pagination.page * pagination.limit + pagination.limit
 		);
-        
-        res.json({
-            result: true,
-            data: todoList,
-            message: "Get TodoList Successfully !"
 
-        })
-    } catch (err) {
-        console.log("Error Get Todo",err);
-        res.json({
-            result: false,
-            message: "Failed Get TodoList !"
+		res.json({
+			result: true,
+			data: todoList,
+			message: 'Get TodoList Successfully !',
+		});
+	} catch (err) {
+		console.log('Error Get Todo', err);
+		res.json({
+			result: false,
+			message: 'Failed Get TodoList !',
+		});
+	}
+};
 
-        })
+export const getTodo = async (req: IUserRequest, res: Response) => {
+	try {
+		const todoId = req.params.id;
+		const userId = req.userId;
+		const todoRepo = getMongoRepository(Todo);
+		const todo: Todo | undefined = await todoRepo.findOne({
+			where: { _id: new ObjectId(todoId), userId: userId },
+		});
+		if (!todo) {
+			res.json({
+				result: false,
+				message: 'Todo not found',
+			});
+		}
+
+		res.json({
+			result: true,
+			data: todo,
+			message: 'Fail to get a Todo',
+		});
+	} catch (err) {
+		res.json({
+			result: false,
+			message: 'Failed to get todo',
+		});
+	}
+};
+//create todo
+export const createTodo = async (req: IUserRequest, res: Response) => {
+	try {
+		const newTodo = req.body;
+		newTodo.startTime = new Date(newTodo.startTime);
+		newTodo.endTime = new Date(newTodo.endTime);
+		const todoRepo = getMongoRepository(Todo);
+		await todoRepo.save(newTodo);
+		res.json({
+			result: true,
+			data: newTodo,
+			message: 'Create Todo Successfully !',
+		});
+	} catch (err) {
+		console.log('Error Create Todo', err);
+		res.json({
+			result: false,
+			message: 'Create Todo Failed !',
+		});
+	}
+};
+
+export const updateTodo = async (req: IUserRequest, res: Response) => {
+	const todoId = req.params.id;
+	const userId = req.userId;
+	const updateTodo: Todo = req.body;
+	updateTodo.startTime = new Date(updateTodo.startTime);
+	updateTodo.endTime = new Date(updateTodo.endTime);
+	const todoRepo = getMongoRepository(Todo);
+
+	try {
+		await todoRepo.findOneAndUpdate(
+			{
+				_id: new ObjectId(todoId),
+				userId: userId,
+			},
+			{ $set: updateTodo }
+		);
+        res.json(new ResponseData("Update success.",updateTodo));
+	} catch (err) {
+        res.json(new ResponseError('Update failed.'));
     }
-}
+};
 
-export const getTodo = async (req: IUserRequest,res : Response) => {
-    try {
-
+export const deleteTodo = async (req: IUserRequest, res: Response) => {
+    try{
         const todoId = req.params.id;
         const userId = req.userId;
         const todoRepo = getMongoRepository(Todo);
-        const todo: Todo | undefined = await todoRepo.findOne({where: {_id: new ObjectId(todoId),userId: userId} });
-        if (!todo) {
-            res.json({
-                result : false,
-                message : "Todo not found",
-            })
-        }
-
-        res.json({
-            result : true,
-            data: todo,
-            message : "Fail to get a Todo"
-        })
-
-    } catch (err) {
-        res.json({
-            result : false,
-            message : "Failed to get todo",
-        })
+        await todoRepo.findOneAndDelete({_id: new ObjectId(todoId),userId: userId});
+        res.json(new ResponseData('Delete success', todoId));
+    }
+    catch(err){
+        res.json(new ResponseError('Delete fail'));
     }
 }
-
-
-
