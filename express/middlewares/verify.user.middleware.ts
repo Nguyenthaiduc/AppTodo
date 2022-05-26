f68a823c251cc52c4c9200ee36b0f88500e1d7ac
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-26 10:13:36 +0700

    add middleware verify user

---
+++ express/middlewares/verify.user.middleware.ts
@@ -0,0 +1,29 @@
+import { Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { User } from '../models';
+import { IUserRequest } from '../interfaces';
+dotenv.config();
+
+const verifyToken = (req: IUserRequest, res: Response, next: NextFunction) => {
+	const token = req.headers['authorization'];
+	if (token !== undefined) {
+		req.token = token;
+		jwt.verify(token, <string>process.env.SECRET_KEY, (err, decoded) => {
+			if (err) {
+				res.json({
+					result: false,
+					message: 'fail to verify token ' + token,
+				});
+			} else {
+				const user = decoded as User;
+				req.userId = user.id;
+				next();
+			}
+		});
+	} else {
+		res.json({ result: false, message: 'token is not exist' });
+	}
+};
+
+export default verifyToken;
