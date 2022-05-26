04c11ac5ebb5b066e5f7bfd11f00488c2fb0eb9d
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-26 09:14:58 +0700

    add database Mongodb NoSQL

--- express/db/index.ts
+++ express/db/index.ts
@@ -1,4 +1,25 @@
-import dotenv from 'dotenv';
 import { createConnection } from 'typeorm';
+import { Todo } from '../models';
+import { User } from '../models';
+
+const connection = async () => {
+
+    try {
+        await createConnection({
+            type: 'mongodb',
+            host: 'localhost',
+            port: 27017,
+            database: 'Todo',
+            useUnifiedTopology: true,
+            entities: [User,Todo]
+        })
+        console.log("Connect Success !")
+
+    } catch (err) {
+        console.log("Connect Database Error",err);
+    }
+}
+
+
 
 
